7b9bf258c2cae475d0f614ccf8f829c8f3ec344f
snan <snanword@gmail.com>
2020-04-18 11:29:39 +0800

    task_tool init

---
+++ task-tool/src/TaskTool.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * TaskTool.php
+ * @des
+ * Created by PhpStorm.
+ * User: Liwenhua
+ * Date: 2020/4/18
+ * Time: 11:26
+ */
+namespace SnanWord\TaskTool;
+
+class TaskTool
+{
+    public function index(){
+        echo 'hello-word';
+    }
+}
---
+++ task-tool/src/crontab/CrontabManage.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * CrontabManage.php
+ * @des
+ * Created by PhpStorm.
+ * User: Liwenhua
+ * Date: 2020/4/18
+ * Time: 11:28
+ */
+
+namespace SnanWord\TaskTool\Crontab;
+
+
+class CrontabManage
+{
+    public function index(){
+
+    }
+}
