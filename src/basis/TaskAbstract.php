a8e4b35b40d3881315012fc02adce2df55b109c4
snan <snanword@gmail.com>
2021-09-23 10:47:54 +0800

    fix task abstract

--- src/basis/TaskAbstract.php
+++ src/basis/TaskAbstract.php
@@ -3,14 +3,16 @@
  * TaskInterface.php
  * @des
  * Created by PhpStorm.
- * User: Liwenhua
+ * User: Snan
  * Date: 2020/4/18
  * Time: 18:42
  */
 
 namespace SnanWord\TaskTool\basis;
 
-
+/**
+ * TaskAbstract
+ */
 abstract class TaskAbstract
 {
     private $task_name    = __CLASS__;
@@ -25,7 +27,8 @@ abstract class TaskAbstract
 
     /**
      * setCrontab
-     * @des    设置crontab时间 //[,分,时,日,月,周]
+     * @des    设置crontab时间 //[分,时,日,月,周]
+     * @des    Setting crontab cycle //[Minute,Hour,day,month,week]
      * @return array
      * @author Snan
      */
